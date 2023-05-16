c3d835907c0c664d6cbede8df13b43f14b094d26
ralph <ralphxwz@gmail.com>
2023-05-16 22:38:08 +0800

    update

--- main.cc
+++ main.cc
@@ -15,12 +15,15 @@ extern YYSTYPE yylval;
 static struct option long_options[] = {
     // options: name, has_args, flag, val
     {"help", no_argument, 0, 'h'},
+    {"dump-tokens", no_argument, 0, 0},
     {0, 0, 0, 0}
 };
 
 void usage(const char* name)
 {
-    printf("usage: %s [files]\n", name);
+    printf("usage: %s [options] file...\n", name);
+    printf("global options:\n");
+    printf("  --dump-tokens    dump tokens and quit.\n");
     exit(1);
 }
 
@@ -28,9 +31,15 @@ int main(int argc, char *argv[])
 {
     int c;
     int opt_index;
+    bool dump_token = false;
 
     while (c = getopt_long(argc, argv, "h", long_options, &opt_index) != -1) {
         switch (c) {
+        case 0:
+            if (opt_index == 1) {
+                dump_token = true;
+                break;
+            }
         case -1:
             break;
         case 'h':
