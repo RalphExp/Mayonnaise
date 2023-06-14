7483f5948719435b2d0910533d459e1b08a13b2e
ralph <ralphxwz@gmail.com>
2023-06-14 18:36:29 +0800

    update

---
+++ include/option.h
@@ -0,0 +1,18 @@
+#ifndef OPTION_H_
+#define OPTION_H_
+
+#include <set>
+#include <string>
+
+#include "ast.h"
+
+using namespace std;
+
+struct Option {
+    Option() : ast_(nullptr) {}
+
+    cbc::AST* ast_;
+    set<string> typename_;
+};
+
+#endif
